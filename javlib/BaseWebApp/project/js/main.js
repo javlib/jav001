420a10dc55c5eb6baaf6e8d85b765e151c12fbfe
javlib <56030701+javlib@users.noreply.github.com>
2019-10-02 17:08:39 +0200

    Revert "weather in"
    This reverts commit a13f254e697c6ec5e1b5d1100cb05d00e952991c.

--- javlib/BaseWebApp/project/js/main.js
+++ javlib/BaseWebApp/project/js/main.js
@@ -1,12 +1,9 @@
-$(document).ready(function(){
+function showPicture(){
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
+  $("p").html("High five! You're building your first web app!");
 
-  getWeather();
-})
-
-function getWeather(){
-
-  var url="api.openweathermap.org/data/2.5/weather?q=Madrid&APPID="+apiKey;
-  $.ajax(url,{success: function(data){
-    console.log(data);
-  }})
+  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+  
 }
