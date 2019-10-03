928d46bae84a096efeff0e7ca59fdb2c4fb4119b
javlib <56030701+javlib@users.noreply.github.com>
2019-10-03 22:34:22 +0200

    summer

--- javlib/BaseWebApp/project/js/main.js
+++ javlib/BaseWebApp/project/js/main.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 
   getWeather();
 })
-function getWeather(){
+function getWeather(arg1){
 
-  var url="https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&APPID="+apikey;
+  var url="https://api.openweathermap.org/data/2.5/weather?q="+arg1+"&units=imperial&APPID="+apikey;
   $.ajax(url,{success:function(data){
 $(".city").text(data.name);
 $(".temp").text(data.main.temp);
   }})
+}
+function searchWeather(){
+  var seekQuery=$(".search").val();
+  getWeather(seekQuery);
 }
