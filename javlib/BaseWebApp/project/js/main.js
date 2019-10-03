bc1023a3df80d23f37e662b28f85d5578979d000
javlib <56030701+javlib@users.noreply.github.com>
2019-10-03 23:22:49 +0200

    hh

--- javlib/BaseWebApp/project/js/main.js
+++ javlib/BaseWebApp/project/js/main.js
@@ -3,11 +3,14 @@ $(document).ready(function(){
   getWeather();
 })
 function getWeather(arg1){
-
+  $(".city").text("");
+  $(".temp").text("");
   var url="https://api.openweathermap.org/data/2.5/weather?q="+arg1+"&units=imperial&APPID="+apikey;
   $.ajax(url,{success:function(data){
 $(".city").text(data.name);
 $(".temp").text(data.main.temp);
+  },error:function(error){
+    $(".error-message").text("An error ocurred.");
   }})
 }
 function searchWeather(){
